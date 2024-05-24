7c9d9219d8aceb8f8ab5bfbb34279ebad34b33d2
Daniel Andrade <135632330+DanielAirees@users.noreply.github.com>
2024-05-24 11:13:05 -0300

    crud.py

--- crud.py
+++ crud.py
@@ -9,7 +9,7 @@ class AppBD:
             self.conexao = conector.connect(database='postgres',
                                    user='postgres',
                                    host='127.0.0.1',
-                                   password='senhadoBD',
+                                   password='sua_senha',
                                    port='5432')
         except (Exception, conector.Error) as erro:
             if self.conexao:
@@ -20,12 +20,30 @@ class AppBD:
         try:
             self.abrir_conexao()
             cursor = self.conexao.cursor()
-            query = """insert into public."colaborador" ("cod", "nome", "cpf", "rg", "emissor", "genero", "data_nascimento",
-                            "ddd", "telefone", "cep", "cidade", "estado", "bairro", "rua", "numero", "complemento", "salario", 
-                            "data_contratacao", "cargo", "area", "nivel") 
-                            values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
-            inserir = (cod, nome, cpf, rg, emissor, genero, data_nascimento, ddd, telefone, cep, cidade, estado, bairro, rua, numero, complemento, salario, data_contratacao, cargo, area, nivel)
-            cursor.execute(query, inserir)
+            # Inserir dados na tabela pessoa
+            cursor.execute("""
+                INSERT INTO pessoa (cod, nome, cpf, rg, emissor, genero, data_nascimento)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """, (cod, nome, cpf, rg, emissor, genero, data_nascimento))
+
+            # Inserir dados na tabela endereco
+            cursor.execute("""
+                INSERT INTO endereco (cod, cep, estado, cidade, bairro, rua, numero, complemento)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            """, (cod, cep, estado, cidade, bairro, rua, numero, complemento))
+
+            # Inserir dados na tabela telefone
+            cursor.execute("""
+                INSERT INTO telefone (cod, ddd, telefone)
+                VALUES (%s, %s, %s)
+            """, (cod, ddd, telefone))
+
+            # Inserir dados na tabela profissional
+            cursor.execute("""
+                INSERT INTO profissional (cod, data_contratacao, cargo, area, nivel, salario)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """, (cod, data_contratacao, cargo, area, nivel, salario))
+
             self.conexao.commit()
             count = cursor.rowcount
             print(count,'dado cadastrado')
@@ -45,7 +63,14 @@ class AppBD:
         try:
             self.abrir_conexao()
             cursor = self.conexao.cursor()
-            query = """select * from public."colaborador" """
+            query = """SELECT 
+            p.cod, p.nome, p.cpf, p.rg, p.emissor, p.genero, p.data_nascimento,
+            t.ddd, t.telefone, e.cep, e.cidade, e.estado, e.bairro, e.rua, e.numero, e.complemento,          
+            pr.salario, pr.data_contratacao, pr.cargo, pr.area, pr.nivel
+            FROM pessoa p
+            LEFT JOIN endereco e ON p.cod = e.cod
+            LEFT JOIN telefone t ON p.cod = t.cod
+            LEFT JOIN profissional pr ON p.cod = pr.cod; """
             cursor.execute(query)
             registros = cursor.fetchall()
             print(registros)
@@ -67,8 +92,11 @@ class AppBD:
         try:
             self.abrir_conexao()
             cursor = self.conexao.cursor()
-            query = """delete from public."colaborador" where "cod" = (%s)"""
-            cursor.execute(query, (cod,))
+            # Deleta os dados nas tabelas relacionadas
+            cursor.execute("DELETE FROM endereco WHERE cod = %s", (cod,))
+            cursor.execute("DELETE FROM telefone WHERE cod = %s", (cod,))
+            cursor.execute("DELETE FROM profissional WHERE cod = %s", (cod,))
+            cursor.execute("DELETE FROM pessoa WHERE cod = %s", (cod,))
             self.conexao.commit()
             count = cursor.rowcount
             print(count, 'registro excluído com sucesso')
@@ -90,26 +118,58 @@ class AppBD:
             self.abrir_conexao()
             cursor = self.conexao.cursor()
             
-            sql_select_query = """select * from public."colaborador" 
-                                        where "cod" = %s"""
-            
-            cursor.execute(sql_select_query, (cod,))
+            query = """SELECT 
+            p.cod, p.nome, p.cpf, p.rg, p.emissor, p.genero, p.data_nascimento,
+            t.ddd, t.telefone, e.cep, e.cidade, e.estado, e.bairro, e.rua, e.numero, e.complemento,          
+            pr.salario, pr.data_contratacao, pr.cargo, pr.area, pr.nivel
+            FROM pessoa p
+            LEFT JOIN endereco e ON p.cod = e.cod
+            LEFT JOIN telefone t ON p.cod = t.cod
+            LEFT JOIN profissional pr ON p.cod = pr.cod; """
+            cursor.execute(query)
             registro = cursor.fetchone()
             print(registro)
             
-            sql_update_query = """Update public."colaborador" set "nome" = %s, 
-            "cpf" = %s, rg= %s, emissor= %s, genero= %s, data_nascimento= %s, ddd = %s, telefone= %s, cep= %s, cidade= %s, 
-                  estado= %s, bairro= %s, rua= %s, numero= %s, complemento= %s, salario= %s, data_contratacao= %s, 
-            cargo= %s, area= %s, nivel= %s where "cod" = %s"""
-            cursor.execute(sql_update_query, (nome, cpf, rg, emissor, genero, data_nascimento, ddd, telefone, cep, cidade, 
-                  estado, bairro, rua, numero, complemento, salario, data_contratacao, cargo, area, nivel, cod))
+            cursor.execute("""
+            UPDATE pessoa 
+            SET nome = %s, cpf = %s, rg = %s, emissor = %s, genero = %s, data_nascimento = %s
+            WHERE cod = %s;
+            """, (nome, cpf, rg, emissor, genero, data_nascimento, cod))
+
+            # Atualiza os dados na tabela endereco
+            cursor.execute("""
+                UPDATE endereco 
+                SET cep = %s, estado = %s, cidade = %s, bairro = %s, rua = %s, numero = %s, complemento = %s
+                WHERE cod = %s;
+            """, (cep, estado, cidade, bairro, rua, numero, complemento, cod))
+
+            # Atualiza os dados na tabela telefone
+            cursor.execute("""
+                UPDATE telefone 
+                SET ddd = %s, telefone = %s
+                WHERE cod = %s;
+            """, (ddd, telefone, cod))
+
+            # Atualiza os dados na tabela profissional
+            cursor.execute("""
+                UPDATE profissional 
+                SET data_contratacao = %s, cargo = %s, area = %s, nivel = %s, salario = %s
+                WHERE cod = %s;
+            """, (data_contratacao, cargo, area, nivel, salario, cod))
+
             self.conexao.commit()
             count = cursor.rowcount
             print(count, "Registro atualizado no BD! ")
             print("Registro Depois da Atualização ")
-            sql_select_query = """select * from public."colaborador" 
-                                    where "cod" = %s"""
-            cursor.execute(sql_select_query, (cod,))
+            query = """SELECT 
+            p.cod, p.nome, p.cpf, p.rg, p.emissor, p.genero, p.data_nascimento,
+            t.ddd, t.telefone, e.cep, e.cidade, e.estado, e.bairro, e.rua, e.numero, e.complemento,          
+            pr.salario, pr.data_contratacao, pr.cargo, pr.area, pr.nivel
+            FROM pessoa p
+            LEFT JOIN endereco e ON p.cod = e.cod
+            LEFT JOIN telefone t ON p.cod = t.cod
+            LEFT JOIN profissional pr ON p.cod = pr.cod; """
+            cursor.execute(query)
             record = cursor.fetchone()
             print(record)
         except (Exception, conector.Error) as error:
